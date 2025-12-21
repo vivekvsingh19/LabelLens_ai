b903f06ed561014acd554c5c0fc5b312ab967812
vivek singh <vivekvsingh19@gmail.com>
2025-12-21 23:33:52 +0530

    v

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/routing/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/providers/ui_providers.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  print("ENV LOADED ROOT: GEMINI_MODEL=${dotenv.env['GEMINI_MODEL']}");
   runApp(const ProviderScope(child: LabelSafeAIApp()));
 }
 
